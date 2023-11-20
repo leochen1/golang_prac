77cec898c944d92031d428999fcf1506ffb33ba3
Leo23 Chen <Leo23.Chen@advantech.com>
2023-11-20 16:04:33 +0800

    03

---
+++ cmd/server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"golang_prac/note"
+)
+
+func main() {
+	fmt.Println("cmd/server")
+	note.SayHelloWorld()
+}
---
+++ note/note.go
@@ -0,0 +1,7 @@
+package note
+
+import "fmt"
+
+func SayHelloWorld() {
+	fmt.Println("Hello, World!")
+}
